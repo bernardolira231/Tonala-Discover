91c7234fd42db38d16f7153ec952e26fb1f2f634
Bernardo Lira <harritolira@gmail.com>
2024-05-28 10:56:09 -0600

    refactor: Update DishItem component to include image and improve styling

--- src/components/DishItem.jsx
+++ src/components/DishItem.jsx
@@ -1,22 +1,45 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 import StyledText from "./StyledText.jsx";
 
 const DishItem = (props) => (
   <View key={props.id} style={styles.container}>
-    <StyledText fontSize={"subheading"} fontWeight={"bold"}>
-      {props.name}
-    </StyledText>
-    <StyledText color={"primary"}>{props.description}</StyledText>
-    <StyledText color={"secondary"}>{props.ingredients.join(", ")}</StyledText>
+    <Image
+      source={{ uri: props.image }}
+      style={{
+        //  width: 80,
+        //  height: 80,
+        //  borderRadius: 80 / 2
+        width: "100%",
+        height: 200,
+      }}
+    />
+    <View style={{ padding: 10 }}>
+      <StyledText fontSize={"subheading"} fontWeight={"bold"}>
+        {props.name}
+      </StyledText>
+      <StyledText color={"primary"} style={{marginVertical: 5,}}>{props.description}</StyledText>
+      <StyledText color={"secondary"}>
+        {props.ingredients.join(", ")}
+      </StyledText>
+    </View>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
-    paddingBottom: 5,
-    paddingTop: 5,
+    // padding: 20,
+    // paddingBottom: 5,
+    // paddingTop: 5,
+    backgroundColor: "#2C3E50",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+    margin: 10,
+    overflow: "hidden",
   },
 });
 
