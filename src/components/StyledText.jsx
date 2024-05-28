c94ebe08caa95e1e6b60684fb1ecc2069ab22184
Bernardo Lira <harritolira@gmail.com>
2024-05-28 08:57:58 -0600

    feat: Add StyledText component for styling text

---
+++ src/components/StyledText.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import theme from "../theme.js";
+import { Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  text: {
+    color: theme.colors.textPrimary,
+    fontSize: theme.fontSizes.body,
+    fontFamily: theme.fonts.main,
+    fontWeight: theme.fontWeights.normal,
+  },
+  colorPrimary: {
+    color: theme.colors.textPrimary,
+  },
+  colorSecondary: {
+    color: theme.colors.textSecondary,
+  },
+  bold: {
+    fontWeight: theme.fontWeights.bold,
+  },
+  subheading: {
+    fontSize: theme.fontSizes.subHeading,
+  },
+  title: {
+    fontSize: theme.fontSizes.title,
+  },
+});
+
+export default function StyledText({
+  children,
+  color,
+  fontSize,
+  fontWeight,
+  style,
+  ...restOfProps
+}) {
+  const textStyle = [
+    styles.text,
+    color === "primary" && styles.colorPrimary,
+    color === "secondary" && styles.colorSecondary,
+    fontSize === "subheading" && styles.subheading,
+    fontSize === "title" && styles.title,
+    fontWeight === "bold" && styles.bold,
+    style,
+  ];
+
+  return (
+    <Text style={textStyle} {...restOfProps}>
+      {children}
+    </Text>
+  );
+}
---
+++ src/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  colors: {
+    textPrimary: "#E0E0E0",
+    textSecondary: "#B0B0B0",
+  },
+  fontSizes: {
+    body: 14,
+    title: 24,
+    subHeading: 20,
+  },
+  fonts: {
+    main: "System",
+  },
+  fontWeights: {
+    normal: "400",
+    bold: "700",
+  },
+};
+
+export default theme;
