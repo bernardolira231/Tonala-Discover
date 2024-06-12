200ed006a4fc8654eabe8c02670ea1a81103132c
Bernardo Lira <harritolira@gmail.com>
2024-06-12 14:51:57 -0600

    feat: Remove ListCards title prop and associated code

--- src/components/ListsCard.jsx
+++ src/components/ListsCard.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Text, FlatList } from 'react-native'
+import { FlatList } from 'react-native'
 import DishItem from './CardItem.jsx'
-import { styles } from '../styles/StyledListCard.jsx'
 
-const ListCards = ({ title, data, map, extraFields = [], type }) => {
+const ListCards = ({ data, map, extraFields = [], type }) => {
   return (
     <FlatList
       nestedScrollEnabled
       data={data}
-      ListHeaderComponent={<Text style={styles.h1}>{title}</Text>}
       renderItem={({ item }) => <DishItem item={item} map={map} extraFields={extraFields} type={type} />}
     />
   )
