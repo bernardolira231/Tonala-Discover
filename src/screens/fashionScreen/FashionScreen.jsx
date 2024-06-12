c650c467b781e25921df06315afe09b83f6498c8
Bernardo Lira <harritolira@gmail.com>
2024-06-12 14:46:04 -0600

    chore: Update FashionScreen and HandicraftScreen components

--- src/screens/fashionScreen/FashionScreen.jsx
+++ src/screens/fashionScreen/FashionScreen.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { styles } from '../../styles/StyledDishesScreen.jsx'
+import { globalStyles } from '../../styles/StyledGlobalScreen.jsx'
+import { StatusBar } from 'expo-status-bar'
+import HandCraft from '../../components/ListsCard.jsx'
+import handicrafts from '../../../data/handicraft.json'
+import { handicraftsmap, extraFields } from '../../constants/handicraftConst.js'
 
 const FashionScreen = () => {
   return (
-    <View>
-      <Text>Fashion Screen</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={[styles.bg, globalStyles.bgColor]}>
+        <HandCraft title='Handicraft' data={handicrafts} map={handicraftsmap} extraFields={extraFields} type='handicraft' />
+        <StatusBar style='dark' />
+      </SafeAreaView>
+    </SafeAreaProvider>
   )
 }
 
--- src/screens/handicraftScreen/HandicraftScreen.jsx
+++ src/screens/handicraftScreen/HandicraftScreen.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
-import { styles } from '../../styles/StyledDishesScreen'
-import { globalStyles } from '../../styles/StyledGlobalScreen'
-import { StatusBar } from 'expo-status-bar'
-import HandCraft from '../../components/ListsCard.jsx'
-import handicrafts from '../../../data/handicraft.json'
-import { handicraftsmap, extraFields } from '../../constants/handicraftConst.js'
+import { Text, View } from 'react-native'
 
 const HandicraftScreen = () => {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={[styles.bg, globalStyles.bgColor]}>
-        <HandCraft title='Handicraft' data={handicrafts} map={handicraftsmap} extraFields={extraFields} type='handicraft' />
-        <StatusBar style='dark' />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <View>
+      <Text>Fashion Screen</Text>
+    </View>
   )
 }
 
