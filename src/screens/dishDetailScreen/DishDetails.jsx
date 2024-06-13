f2d0bf91b152edc8ccf584a754a554a2eb6cc2bb
Bernardo Lira <harritolira@gmail.com>
2024-06-12 19:29:32 -0600

    feat: Add ScrollView to DishDetails screen for better user experience

--- src/screens/dishDetailScreen/DishDetails.jsx
+++ src/screens/dishDetailScreen/DishDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import { styles } from '../../styles/StyledDishDetailsScreen'
 import { globalStyles } from '../../styles/StyledGlobalScreen'
@@ -10,24 +10,26 @@ const DishDetails = ({ route }) => {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={[styles.container, globalStyles.bgColor]}>
-        <View style={styles.imageContainer}>
-          <Image source={{ uri: image }} style={styles.image} />
-        </View>
-        <View style={styles.header}>
-          <View style={styles.firstpart}>
-            <Text style={styles.title}>{name}</Text>
-            <Text style={styles.description}>{description}</Text>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={styles.imageContainer}>
+            <Image source={{ uri: image }} style={styles.image} />
           </View>
-          <View style={styles.secondpart}>
-            <Text style={styles.subtitle}>Ingredients:</Text>
-            {ingredients.map((ingredient, index) => (
-              <Text key={index} style={styles.ingredients}>
-                {ingredient}
-              </Text>
-            ))}
-          </View>
+          <View style={styles.header}>
+            <View style={styles.firstpart}>
+              <Text style={styles.title}>{name}</Text>
+              <Text style={styles.description}>{description}</Text>
+            </View>
+            <View style={styles.secondpart}>
+              <Text style={styles.subtitle}>Ingredients:</Text>
+              {ingredients.map((ingredient, index) => (
+                <Text key={index} style={styles.ingredients}>
+                  {ingredient}
+                </Text>
+              ))}
+            </View>
 
-        </View>
+          </View>
+        </ScrollView>
       </SafeAreaView>
     </SafeAreaProvider>
   )
--- src/styles/StyledMap.jsx
+++ src/styles/StyledMap.jsx
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native'
 export const styles = StyleSheet.create({
   container: {
     width: '100%',
-    height: 400,
+    height: 300,
     borderRadius: 15,
     overflow: 'hidden',
     marginTop: 30
